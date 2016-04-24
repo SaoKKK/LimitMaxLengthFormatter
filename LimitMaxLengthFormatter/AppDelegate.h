bc732254174de9c84fd0df0c89c5010e3f3110fb
Saori Kohno <saorikohno@yahoo.com>
2016-04-25 06:27:56 +0900

    Initial Commit

---
+++ LimitMaxLengthFormatter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LimitMaxLengthFormatter
+//
+//  Created by 河野 さおり on 2016/04/25.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LimitMaxLengthFormatter/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LimitMaxLengthFormatter
+//
+//  Created by 河野 さおり on 2016/04/25.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
